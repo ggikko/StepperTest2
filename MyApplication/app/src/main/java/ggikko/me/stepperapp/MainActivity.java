482fa7cc0884504caf8b7b4bff114fe05e7bd73c
Jihong Park <ggikko@hotmail.com>
2016-05-12 17:49:35 +0900

    add bignerdranch.expandablerecyclerview library

--- MyApplication/app/src/main/java/ggikko/me/stepperapp/MainActivity.java
+++ MyApplication/app/src/main/java/ggikko/me/stepperapp/MainActivity.java
@@ -1,7 +1,11 @@
 package ggikko.me.stepperapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ggikko.me.stepperapp.fragment.InvestRegistrationFragmentThird;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new InvestRegistrationFragmentThird();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
     }
 }
---
+++ MyApplication/app/src/main/java/ggikko/me/stepperapp/fragment/InvestRegistrationFragmentThird.java
@@ -0,0 +1,33 @@
+package ggikko.me.stepperapp.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ggikko.me.stepperapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvestRegistrationFragmentThird extends Fragment {
+
+
+    public InvestRegistrationFragmentThird() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_invest_registration_fragment_third, container, false);
+
+
+
+        return view;
+    }
+
+}
